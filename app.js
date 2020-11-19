a8364172d2f916e04eef8f45811c31d9b8569d1f
Manas Hejmadi | Developer <67999871+synapsecode@users.noreply.github.com>
2020-11-19 18:02:57 +0530

    Alpha1

---
+++ app.js
@@ -0,0 +1,15 @@
+//Handles the Redirecting to Contact sources
+const handleContactClick = (name) => {
+	console.log("Redirecting to: " + name);
+}
+
+//Handles the Work With Me Button
+const workWithMe = () => {
+	console.log("Redirecting to Work With Me!");
+	alert("Coming Soon.....");
+}
+
+//Scrolls to different sections in the WebPage
+const goto = (name) => {
+	console.log("Scrolling To to " + name);
+}
