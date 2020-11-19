b2e5b797ff2642d224f17b6e2ae19f9f5bdd0773
Manas Hejmadi | Developer <67999871+synapsecode@users.noreply.github.com>
2020-11-20 00:27:57 +0530

    Added 'More' Section and implemented navbar

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let checked = false;
+
 //Handles the Redirecting to Contact sources
 const handleContactClick = (name) => {
 	console.log("Redirecting to: " + name);
@@ -12,4 +14,28 @@ const workWithMe = () => {
 //Scrolls to different sections in the WebPage
 const goto = (name) => {
 	console.log("Scrolling To to " + name);
+	// document.getElementById(name).scrollIntoView({behavior: 'smooth'});
+	if(checked){
+		document.getElementById("click").checked = false;
+	}
+	checked = false;
+	window.location.href = "#"+name;
+
+	
 }
+
+
+const gotoproject = (name) => {
+	alert("Going to " + name);
+}
+
+
+//Prevents Navbar from Disappearing on InPage Redirect
+var shiftWindow = function() { scrollBy(0, -70) };
+if (location.hash) shiftWindow();
+window.addEventListener("hashchange", shiftWindow);
+
+
+document.getElementById("click").addEventListener("change", function(x){
+	checked = !checked;
+})
